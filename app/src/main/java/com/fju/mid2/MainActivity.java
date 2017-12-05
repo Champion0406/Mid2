e58f67102b5b4f74516eca44865dfa12ff487ac6
champion0406 <barney0406@gmail.com>
2017-12-05 11:35:21 +0800

    12/5

--- app/src/main/java/com/fju/mid2/MainActivity.java
+++ app/src/main/java/com/fju/mid2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.fju.mid2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    public void gotofunc1(View v)
+    {
+        Intent intent = new Intent(this, Func1Activity.class);
+        startActivity(intent);
+    }
+    public void gotofunc2(View v)
+    {
+        Intent intent = new Intent(this, Func2Activity.class);
+        startActivity(intent);
+    }
+
+    public void gotofunc3(View v)
+    {
+        Intent intent = new Intent(this, Func3Activity.class);
+        startActivity(intent);
+        Toast.makeText(this,"特殊功能",Toast.LENGTH_LONG).show();
+        finish();
+    }
+    public void gotofunc4(View v)
+    {
+        Intent intent = new Intent(this, Func4Activity.class);
+        startActivity(intent);
+    }
+    public void gotofunc5(View v)
+    {
+        Intent intent = new Intent(this, Func5Activity.class);
+        startActivity(intent);
+    }
+    public void gotofunc6(View v)
+    {
+        Intent intent = new Intent(this, Func6Activity.class);
+        startActivity(intent);
+    }
+    public void gotofunc7(View v)
+    {
+        Intent intent = new Intent(this, Func7Activity.class);
+        startActivity(intent);
+    }
+    public void gotofunc8(View v)
+    {
+        Intent intent = new Intent(this, Func8Activity.class);
+        startActivity(intent);
+    }
+
+
+
 }
